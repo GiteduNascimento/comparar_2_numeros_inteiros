33cc99f4893d2194a5d7ab0f1e794b5370a885cd
C4L05 <141598895+GiteduNascimento@users.noreply.github.com>
2023-08-12 00:05:15 -0300

    Compar 2 numeros inteiros

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+int num2;
+
+Console.WriteLine("Digite o primeiro número:");
+num1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+num2= int.Parse(Console.ReadLine());
+
+if(num1==num2){
+    Console.WriteLine("Os números são iguais");
+}
+else if(num1!=num2){
+    Console.WriteLine("Os números não são iguais");
+}else{
+    Console.WriteLine("Tente novamente...");
+}
